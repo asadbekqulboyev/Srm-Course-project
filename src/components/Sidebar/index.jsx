1cb3a473b8cf442c3432749b1a31718d19651475
asadbek <asadbekqulboyev@gmail.com>
2024-08-31 16:19:45 +0500

    Sidebar-2

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,28 +1,74 @@
 import React from 'react'
 import { Outlet, useNavigate } from 'react-router-dom'
 import Profile from './profile'
-import { Body, Container, Side,Wrapper,Logo,LogOut} from './style'
+import { Body, Container, Side, Wrapper, Logo, LogOut,Menu ,MenuItem,Arrow,ChildWrapper} from './style'
 import Navbar from '../Navbar'
-
+import sidebar from '../../utils/sidebar'
+import { useState } from 'react'
 function Sidebar() {
+  const [open,setOpen]=useState([])
   const navigate = useNavigate()
-  const logoClick = ()=>{
+  const logoClick = () => {
     navigate('/');
+  }
+  const onClickParent= (id)=>{
+    if(open.includes(id)){
+
+      let data = open.filter((val)=>val!==id)
+      setOpen(data)
+    }else{
+      setOpen([...open,id])
+    }
+    console.log(open);
+    
   }
   return (
     <Container>
       <Side>
         <Logo onClick={logoClick}>Crm webbrain</Logo>
-        <Profile/>
+        <Profile />
+        <Menu>
+          {
+            sidebar.map((parent) => {
+              const {icon: Icon}=parent
+              // console.log(Icon);
+              let actives= open.includes(parent.id)
+              return (
+                <> 
+                <MenuItem key={parent.id} onClick={()=>onClickParent(parent.id)}>
+                 <MenuItem.Title>
+                 {Icon && <Icon/>}
+                 {parent.title}
+                 </MenuItem.Title>
+                 {parent?.children?.length&&<Arrow active={actives} />}
+                </MenuItem>
+                <ChildWrapper active={actives}>
+                {parent?.children?.map((child)=>{
+                  return <MenuItem key={child.id}>
+                <MenuItem.Title> {child?.title}</MenuItem.Title> 
+                  </MenuItem>
+                  
+
+                })}
+                </ChildWrapper>
+                
+                </>
+               
+            )
+            })
+          }
+
+        </Menu>
+
         <LogOut>Chiqish</LogOut>
-        
+
       </Side>
-      
+
 
       <Body>
         <Navbar>Navbar</Navbar>
         <Wrapper>
-        <Outlet />
+          <Outlet />
 
         </Wrapper>
       </Body>
