2ea67647a79fcd154c72b1428bb1820facd4c74f
asadbek <asadbekqulboyev@gmail.com>
2024-10-04 07:21:40 +0500

    Checkin_status

--- src/components/Guruhlar/Guruhlar/Checking.jsx
+++ src/components/Guruhlar/Guruhlar/Checking.jsx
@@ -1,23 +1,39 @@
 import GenericTable from '../../Generics/Table';
-import {  Container } from "./style"
+import { Container, Icon } from "./style"
 import BreadCrumbs from "../../Generics/BreadCrumbs";
 import GenericButton from "../../Generics/Button";
 import { students } from '../../../mock/students';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { useState } from 'react';
-export const Checking = () => {
-    const [rows,setRows]=useState(students)
-    const headCells = [
-        { id: 'name', label: 'Ismi' },
-        { id: 'phone', label: 'Telefoni' }
-    ]
+import Status from './Status';
 
+
+export const Checking = () => {
+    const [rows, setRows] = useState(students)
+    const headCells = [
+        { id: 'name', label: 'Ismi'},
+        { id: 'phone', label: 'Telefoni'},
+        { id: '25-09', label: '25-09', render:(props)=>{
+            return <Status value={props['25-09']}/>    } ,align:'center'},
+        { id: '26-09', label: '26-09', render:(props)=>{
+            return <Status value={props['26-09']}/>    } ,align:'center'},
+        { id: '27-09', label: '27-09', render:(props)=>{
+            return <Status value={props['27-09']}/>    } ,align:'center'},
+        { id: '28-09', label: '28-09', render:(props)=>{
+            return <Status value={props['28-09']}/>    } ,align:'center'},
+        { id: '29-09', label: '29-09', render:(props)=>{
+            return <Status value={props['29-09']}/>    } ,align:'center'},
+        { id: '30-09', label: '30-09', render:(props)=>{
+            return <Status value={props['30-09']}/>    } ,align:'center'},
+    ]
+    // const onStatusClick = ()=>{
+    // }
     const data1 = []
     return (
         <Container>
-           <BreadCrumbs>
-           <LocalizationProvider dateAdapter={AdapterDayjs} style={{ top: '-16px' }}>
+            <BreadCrumbs>
+                <LocalizationProvider dateAdapter={AdapterDayjs} style={{ top: '-16px' }}>
                     <DatePicker
                         style={{ padding: '0', width: '270px', flex: '0 0 auto' }}
                     />
--- src/mock/students.js
+++ src/mock/students.js
@@ -1,6 +1,6 @@
 export const students = [
-    {id:1,phone:'+99899 233 00-30',name:'Holmatov Toshmat'},
-    {id:2,phone:'+99899 233 00-30',name:'Abdurayimov Sherri'},
-    {id:3,phone:'+99899 233 00-30',name:'Shermat Polvon'},
-    {id:4,phone:'+99899 233 00-30',name:'Holmatov Qulmat'},
+    { id: 1, phone: '+99899 233 00-30', name: 'Holmatov Toshmat', '25-09': 'keldi', '26-09': 'keldi', '27-09': 'birinchi', '28-09': 'sababsiz', '29-09': 'keldi', '30-09': 'keldi' },
+    { id: 2, phone: '+99899 233 00-30', name: 'Abdurayimov Sherri', '25-09': 'keldi', '26-09': 'keldi', '27-09': 'keldi', '28-09': 'keldi', '29-09': 'keldi', '30-09': 'keldi' },
+    { id: 3, phone: '+99899 233 00-30', name: 'Shermat Polvon', '25-09': 'keldi', '26-09': 'keldi', '27-09': 'sbabli', '28-09': 'keldi', '29-09': 'keldi', '30-09': 'keldi' },
+    { id: 4, phone: '+99899 233 00-30', name: 'Holmatov Qulmat', '25-09': 'keldi', '26-09': 'keldi', '27-09': 'sababsiz', '28-09': 'keldi', '29-09': 'keldi', '30-09': 'keldi' },
 ]
